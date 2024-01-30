8d93fc403896f7d53cd85b07f61d257e85cb665a
joel danjuma <leojjad@gmail.com>
2024-01-30 18:46:21 +0100

    Initial Commit

---
+++ public/icons/facebook.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { IconSvgProps } from "@/types/typings";
+
+export const Facebook : React.FC<IconSvgProps> = () => (
+                      <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_305_3957)">
+<path d="M7.60334 17.0363V10.072H5.21289V6.8577H7.60334V4.61305C7.60334 2.16793 9.15128 0.964844 11.3325 0.964844C12.3773 0.964844 13.2753 1.04263 13.537 1.0774V3.63269L12.0242 3.63338C10.8379 3.63338 10.57 4.19708 10.57 5.02426V6.8577H13.7843L12.7129 10.072H10.57V17.0363H7.60334Z" fill="#808080"/>
+</g>
+<defs>
+<clipPath id="clip0_305_3957">
+<rect width="17.1429" height="17.1429" fill="white" transform="translate(0.927734 0.428711)"/>
+</clipPath>
+</defs>
+</svg>
+)
+
