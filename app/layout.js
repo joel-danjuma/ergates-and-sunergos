fe01b12119e6ea6b43b371da6dff5e83a8f3b3ca
joel danjuma <leojjad@gmail.com>
2024-01-30 18:41:50 +0100

    Initial Commit

---
+++ app/layout.js
@@ -0,0 +1,61 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+import {siteConfig} from "../config/site"
+import Nav from  "../components/navbar"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_URL}`),
+  alternates: {
+      canonical: "/",
+      languages: {
+          "en-US": "/en-US",
+      },
+  },
+  title: {
+      default: siteConfig.name,
+      template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+      icon: "/favicon.ico",
+      shortcut: "/favicon.ico",
+  },
+  openGraph: {
+      title: siteConfig.name,
+      description: siteConfig.description,
+      type: "website",
+      siteName: "DAWN AI",
+      url: `${process.env.NEXT_PUBLIC_URL}`,
+  },
+  robots: {
+      index: true,
+      follow: true,
+      nocache: false,
+      googleBot: {
+          index: true,
+          follow: false,
+          noimageindex: true,
+          "max-video-preview": -1,
+          "max-image-preview": "large",
+          "max-snippet": -1,
+      },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="">
+      <body className={`${inter.className}]`}>
+        <Providers>
+          <Nav />
+          <main className="max-w-[1440px]">
+            {children}
+          </main>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,15 @@
+import SocialProof from "@/components/socialProof";
+import Hero from "../components/hero";
+import ValueProps from "@/components/valueProps";
+import Services from "@/components/services";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <SocialProof />
+      <ValueProps />
+      <Services />
+    </>
+  );
+}
