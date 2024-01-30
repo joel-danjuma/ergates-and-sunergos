1e853b35a659c7a48e9648886dccbb7cce12073b
joel danjuma <leojjad@gmail.com>
2024-01-30 18:42:39 +0100

    Initial Commit

---
+++ config/site.js
@@ -0,0 +1,48 @@
+export const siteConfig = {
+    name: "Ergates and Sunergos | Leadership Excellence and Transformative Management Training for Peak Performance",
+    description:
+        "Elevate your team with innovative, flexible training for lasting transformation. Acquire skills for outstanding performance and organizational success.",
+    navItems: [
+        {
+            label: "About Us",
+            href: "/about",
+        },
+        {
+            label: "Courses",
+            href: "/courses",
+        },
+        {
+            label: "Events",
+            href: "/events",
+        },
+
+       
+    ],
+    navMenuItems: [
+    
+        {
+            label: "About Us",
+            href: "/about",
+        },
+        {
+            label: "Courses",
+            href: "/courses",
+        },
+        {
+            label: "Events",
+            href: "/events",
+        },
+      
+        // {
+        //     label: "Logout",
+        //     href: "/logout",
+        // },
+    ],
+    links: {
+        github: "https://github.com/nextui-org/nextui",
+        twitter: "https://twitter.com/getnextui",
+        // docs: "https://nextui.org",
+        // discord: "https://discord.gg/9b6yyZKmH4",
+        // sponsor: "https://patreon.com/jrgarciadev",
+    },
+}
